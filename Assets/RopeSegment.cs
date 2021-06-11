18a29ad6dc526f38574b25dc32978f75794a9efa
kirbyydoge <aqwoguz@gmail.com>
2021-06-12 01:33:28 +0300

    Initial commit

---
+++ Assets/RopeSegment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSegment : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
